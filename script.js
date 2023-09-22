57b7bf1eb0eef4cb998ecf1a956a05aa727f6742
purnima11apr <141928600+purnima11apr@users.noreply.github.com>
2023-09-22 16:54:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
 function getFormvalue() {
     //Write your code here
+	 let form = document.getElementById("form1");
+
+	 var firstName = form.elements["fname"].value;
+    var lastName = form.elements["lname"].value;
+
+	  if (firstName && lastName) {
+
+	  alert("First Name: " + firstName + "\nLast Name: " + lastName);
+    } else {
+
+	alert("Please fill in both First Name and Last Name fields.");
+    }
+
+	 return false;
 
 }
